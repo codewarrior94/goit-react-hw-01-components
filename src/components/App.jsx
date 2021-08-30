6d50ff43408c6a1ce658da4abec7a2ee7724b585
Dmitriy Troitskiy <dmitriysabbath@gmail.com>
2021-08-31 01:29:58 +0300

    Добавил нормалайз и добавил правильно пропсы

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,17 @@
+import 'modern-normalize/modern-normalize.css';
 import { Profile } from './Profile/Profile';
 import user from '../user.json';
 
 export const App = () => {
   return (
     <>
-      <Profile user={user} />
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </>
   );
 };
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,28 +1,28 @@
 import PropTypes from 'prop-types';
 import css from './Profile.module.css';
 
-export const Profile = ({ user }) => {
+export const Profile = ({ name, tag, location, avatar, stats }) => {
   return (
     <div className={css.profile}>
       <div className="description">
-        <img src={user.avatar} alt="Аватар пользователя" className="avatar" />
-        <p className="name">{user.name}</p>
-        <p className="tag">{user.tag}</p>
-        <p className="location">{user.location}</p>
+        <img src={avatar} alt="Аватар пользователя" className="avatar" />
+        <p className="name">{name}</p>
+        <p className="tag">{tag}</p>
+        <p className="location">{location}</p>
       </div>
 
       <ul className="stats">
         <li>
           <span className="label">Followers</span>
-          <span className="quantity">{user.stats.followers}</span>
+          <span className="quantity">{stats.followers}</span>
         </li>
         <li>
           <span className="label">Views</span>
-          <span className="quantity">{user.stats.views}</span>
+          <span className="quantity">{stats.views}</span>
         </li>
         <li>
           <span className="label">Likes</span>
-          <span className="quantity">{user.stats.likes}</span>
+          <span className="quantity">{stats.likes}</span>
         </li>
       </ul>
     </div>
@@ -30,5 +30,13 @@ export const Profile = ({ user }) => {
 };
 
 Profile.propTypes = {
-  user: PropTypes.object.isRequired,
+  name: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.shape({
+    followers: PropTypes.number,
+    views: PropTypes.number,
+    likes: PropTypes.number,
+  }),
 };
