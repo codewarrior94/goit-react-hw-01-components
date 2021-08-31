998449d46d5568e6c2dee3e19c7524c0f07fad96
Dmitriy Troitskiy <dmitriysabbath@gmail.com>
2021-08-31 18:17:22 +0300

    Изменил компоненты чтоб были как в ТЗ

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,7 +14,9 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
-      <Statistics title="Upload stats" stats={statisticalData} />;
+      <Statistics title="Upload stats" stats={statisticalData} />
+      <Statistics stats={statisticalData} />
+
     </>
   );
 };
