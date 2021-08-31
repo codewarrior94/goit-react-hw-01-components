7cf8f213db5560fc8fcfec8708534b407bf9df12
Dmitriy Troitskiy <dmitriysabbath@gmail.com>
2021-08-31 22:26:30 +0300

    Добавил рендер разметки второй задачи

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,21 @@
 import 'modern-normalize/modern-normalize.css';
 import { Profile } from './Profile/Profile';
-import { Statistics } from './Statistics/Statistics'
+import { Statistics } from './Statistics/Statistics';
 import user from '../user.json';
-import statisticalData from '../statistical-data.json'
+import statisticalData from '../statistical-data.json';
 
 export const App = () => {
   return (
     <>
-      <Profile
+      {/* <Profile
         name={user.name}
         tag={user.tag}
         location={user.location}
         avatar={user.avatar}
         stats={user.stats}
-      />
+      /> */}
       <Statistics title="Upload stats" stats={statisticalData} />
       <Statistics stats={statisticalData} />
-
     </>
   );
 };
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,39 +1,31 @@
-import PropTypes from 'prop-types'
-import css from './Statistics.module.css'
+import PropTypes from 'prop-types';
+import css from './Statistics.module.css';
 
-export const Statistics = ({ title='', stats }) => {
+export const Statistics = ({ title = '', stats }) => {
   return (
-        <section className={css.statistics}>
-      <h2 className={css.title}>{title}</h2>
+    <section className="statistics">
+      <h2 className="title">{title}</h2>
 
-        <ul className={css.statList}>
-          <li className={css.item}>
-            <span className={css.label}>.docx</span>
-            <span className={css.percentage}>4%</span>
-          </li>
-          <li className={css.item}>
-            <span className={css.label}>.mp3</span>
-            <span className={css.percentage}>14%</span>
-          </li>
-          <li className={css.item}>
-            <span className={css.label}>.pdf</span>
-            <span className={css.percentage}>41%</span>
-          </li>
-          <li className={css.item}>
-            <span className={css.label}>.mp4</span>
-            <span className={css.percentage}>12%</span>
-          </li>
-        </ul>
-      </section>
-      )
-    }
-
-
+      <ul className="stat-list">
+        {stats.map(listElement => {
+          return (
+            <li className="item">
+              <span className="label">{listElement.label}</span>
+              <span className="percentage">{listElement.percentage}</span>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
+};
 
 Statistics.propTypes = {
-  stats: PropTypes.arrayOf(PropTypes.shape({
-    id: PropTypes.string.isRequired,
-    label: PropTypes.string.isRequired,
-    percentage: PropTypes.number.isRequired,
-  }).isRequired,),
-}
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    }).isRequired,
+  ),
+};
